28934c3347cb08310013edd1a932e8206d26be81
Alex Harrison <alexkendall.harrison@gmail.com>
2016-04-26 11:39:29 -0400

    user can now fetch tracks by query

--- index.ios.js
+++ index.ios.js
@@ -4,21 +4,68 @@
  * @flow
  */
 
+// required imports
 import React, {
   AppRegistry,
   Component,
   StyleSheet,
   Text,
-  View
+  View,
+  ListView,
+  TextInput,
 } from 'react-native';
-
 var Player = require("./player.ios")
 
+// list view variables
+var listDS = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+
+
 class SCPlayer extends Component {
+  constructor(props) {
+    super(props)
+		this.state = {
+      track_models: [],
+	 	}
+  }
   render() {
+    listDS.cloneWithRows(this.state.track_models)
     return (
-      <Player/>
+      <View style={styles.container}>
+        <Player/>
+        <TextInput
+          style={styles.input}
+          onChangeText={(text) => this.fetchData(text)}
+          placeholder="SEARCH TRACKS"
+        />
+        <ListView style={styles.listView}
+            dataSource={listDS.cloneWithRows(this.state.track_models)}
+            renderRow={this.renderTrack}
+         />
+      </View>
     );
+  }
+
+  renderTrack(track){
+    return (
+      <View style={styles.trackView}>
+        <Text>{track.title}</Text>
+      </View>
+    );
+  }
+
+  fetchData(query) {
+    https://api.soundcloud.com/tracks.json?q=\(formattedQuery)&client_id=\(clientId)&limit=50
+    let formattedQuery = query.replace(/\s+/g, '+');
+    let requestBase = "https://api.soundcloud.com/tracks.json?q="
+    let clientAuthExt = "&client_id=b9c5fb9e9a2a62aa8de88e2bff32580f&limit=50"
+    let requestURL = requestBase + formattedQuery + clientAuthExt
+    fetch(requestURL)
+          .then((response) => response.json())
+          .then((responseData) => {
+            this.setState({
+              track_models: responseData
+            })
+          })
   }
 }
 
@@ -27,18 +74,21 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  listView: {
+    marginTop: 20,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  trackView: {
+    backgroundColor: 'white',
   },
+  input: {
+    height: 40,
+    borderColor: 'black',
+    borderWidth: 1,
+    marginTop: 50,
+    marginRight: 20,
+    marginLeft: 20,
+  }
 });
 
 AppRegistry.registerComponent('SCPlayer', () => SCPlayer);
--- player.ios.js
+++ player.ios.js
@@ -5,7 +5,6 @@ import React, {
   Component,
   TouchableOpacity,
   Dimensions,
-  Animated,
   Image
 } from 'react-native';
 let clientId = "b9c5fb9e9a2a62aa8de88e2bff32580f"
@@ -14,7 +13,7 @@ var audioPlayer = require('react-native').NativeModules.RNAudioPlayerURL;
 class Player extends Component {
   constructor(props) {
     super(props)
-    if this.props.streamURL {
+    if (this.props.streamURL) {
       audioPlayer.initWithURL("https://api.soundcloud.com/tracks/13158665/stream?client_id=b9c5fb9e9a2a62aa8de88e2bff32580f") // temporary url to show we can play songs with audio pkayer module
       audioPlayer.play()
     }
