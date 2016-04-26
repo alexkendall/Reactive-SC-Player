a8ee9aa814aa9907727d1a83843e421ff756de4b
Alex Harrison <alexkendall.harrison@gmail.com>
2016-04-26 17:08:24 -0400

    user can now play audio in player component

--- player.ios.js
+++ player.ios.js
@@ -20,14 +20,20 @@ class Player extends Component {
 	}
   render()
   {
+
     if (this.props.track) {
+      let streamURL = this.props.track.stream_url + "?client_id=b9c5fb9e9a2a62aa8de88e2bff32580f"
+      console.log(streamURL)
+      audioPlayer.initWithURL(streamURL) // temporary url to show we can play songs with audio pkayer module
+      audioPlayer.play()
       var artwork_url = "http://3.bp.blogspot.com/-PzpJFD56NmM/U4OEGvGR5pI/AAAAAAAAIO8/s9UBNaw800A/s1600/soundcloud.png"
         if(this.props.track.artwork_url != null) {
           artwork_url = this.props.track.artwork_url
         }
         return (
           <View style={styles.container}>
-            <Text>{this.props.track.title}</Text>
+            <Text style={styles.title}>{this.props.track.title}</Text>
+            <Text style={styles.username}>{this.props.track.user.username}</Text>
             <Image style={styles.artwork} source={{uri: artwork_url}}/>
           </View>
         )
@@ -53,6 +59,16 @@ var styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: 'white',
   },
+  title: {
+    marginTop: 20,
+    fontSize: 20,
+    color: 'black',
+    fontWeight: '600'
+  },
+  username: {
+    fontSize: 14,
+    color: 'gray'
+  }
 });
 
 module.exports = Player
